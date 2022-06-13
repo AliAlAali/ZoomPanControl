177e7fb17cc705d5063fcbfb5d17ac7cae1ee5bc
Ali Alaali <ali.aali2009@hotmail.com>
2022-06-13 11:38:38 +0300

    add OnDraw event for convience

--- Form1.cs
+++ Form1.cs
@@ -12,22 +12,30 @@ namespace ZoomPanControl
 {
     public partial class Form1 : Form
     {
+        IEnumerable<PointF> Points;
         public Form1()
         {
             InitializeComponent();
 
-            var points = new List<PointF>() {
+            Points = new List<PointF>() {
                 new PointF(10, 10),
                 new PointF(10, 80),
                 new PointF(100, 40),
                 new PointF(10, 10),
             };
 
-            zoomControl1.Points = points;
+            zoomControl1.OnDraw += ZoomControl1_OnDraw;
+            zoomControl1.MinZoom = 0.1f;
+            zoomControl1.MaxZoom = 20f;
+            zoomControl1.ZoomSensitivity = 0.2f;
             zoomControl1.Invalidate();
         }
 
-
-
+        private void ZoomControl1_OnDraw(object sender, ZoomControl.DrawEventArgs e)
+        {
+            var graphics = e.Graphics;
+            var pen = new Pen(Color.Black);
+            graphics.DrawLines(pen, Points.ToArray());
+        }
     }
 }
--- ZoomControl.cs
+++ ZoomControl.cs
@@ -12,22 +12,25 @@ using System.Windows.Forms;
 
 namespace ZoomPanControl
 {
+    /// <summary>
+    ///  User control which allows zoom (ctrl + mouse wheel) & pan (shift + mouse left button)
+    ///  double click mouse middle button to reset to original view.
+    /// </summary>
     public partial class ZoomControl : UserControl
     {
 
-        public float ZoomSensitivity { get; set; } = 0.2f;
+        public float ZoomSensitivity { get; set; } = 0.2f; /// Controls how much zoom per zoom mouse scroll
         public float MaxZoom { get; set; } = 20;
         public float MinZoom { get; set; } = 0.1f;
-        public double ZoomScale { get; set; } = 1;
-        public IEnumerable<PointF> Points;
 
+        public event EventHandler<DrawEventArgs> OnDraw; /// Called on everytime control is painted and after zoom calculations. Can be used to draw custom paints.
 
-
-        private PointF Origin = new PointF(0, 0);
+        public double ZoomScale { get; set; } = 1; /// Current Zoom value
+        private PointF Origin = new PointF(0, 0); /// Origin is the left most point of a viewport
         private PointF LastMousePosition = new PointF(-1, -1);
         private MouseWheelEvent MouseWheelData { get; set; }
         private MouseMoveEvent MouseMoveData { get; set; }
-        private Matrix Transformation { get; set; }
+        private Matrix Transformation { get; set; } /// Keeps track of zoom transformations
 
         public ZoomControl() : base()
         {
@@ -40,6 +43,13 @@ namespace ZoomPanControl
             this.Paint += new PaintEventHandler(ZoomControl_Paint);
 
             Transformation = new Matrix();
+
+            // Utilize double buffer to smooth out flickering when re-painting
+            this.SetStyle(
+                System.Windows.Forms.ControlStyles.UserPaint |
+                System.Windows.Forms.ControlStyles.AllPaintingInWmPaint |
+                System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer,
+                true);
         }
 
         private void ZoomControl_MouseDown(object sender, MouseEventArgs e)
@@ -47,12 +57,6 @@ namespace ZoomPanControl
             var mouseX = e.Location.X;
             var mouseY = e.Location.Y;
             LastMousePosition = new PointF(mouseX, mouseY);
-
-            this.SetStyle(
-                System.Windows.Forms.ControlStyles.UserPaint |
-                System.Windows.Forms.ControlStyles.AllPaintingInWmPaint |
-                System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer,
-                true);
         }
 
         private void ZoomControl_MouseMove(object sender, MouseEventArgs e)
@@ -97,14 +101,18 @@ namespace ZoomPanControl
                 var mouseY = MouseWheelData.Position.Y;
                 var zoom = (float)MouseWheelData.Zoom;
 
-                g.TranslateTransform(Origin.X, Origin.Y);
-                Origin.X -= (float)(mouseX / (ZoomScale * zoom) - mouseX / ZoomScale);
-                Origin.Y -= (float)(mouseY / (ZoomScale * zoom) - mouseY / ZoomScale);
+                var newZoom = zoom * ZoomScale;
+                if (MinZoom <= newZoom && newZoom <= MaxZoom)
+                {
+                    g.TranslateTransform(Origin.X, Origin.Y);
+                    Origin.X -= (float)(mouseX / (ZoomScale * zoom) - mouseX / ZoomScale);
+                    Origin.Y -= (float)(mouseY / (ZoomScale * zoom) - mouseY / ZoomScale);
 
-                g.ScaleTransform(zoom, zoom);
-                g.TranslateTransform(-Origin.X, -Origin.Y);
+                    g.ScaleTransform(zoom, zoom);
+                    g.TranslateTransform(-Origin.X, -Origin.Y);
 
-                ZoomScale *= zoom;
+                    ZoomScale *= zoom;
+                }
 
                 // Nullify to prevent unecessary calculations
                 MouseWheelData = null;
@@ -130,28 +138,38 @@ namespace ZoomPanControl
             var brush = new SolidBrush(Color.LightGray);
             var pen = new Pen(Color.Black);
 
-
-
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
             g.FillRectangle(brush, Origin.X, Origin.Y, (float)(width / ZoomScale), (float)(height / ZoomScale));
-            //g.DrawRectangle(pen, new Rectangle(50, 50, 100, 100));
-            if (Points != null && Points.Count() > 0)
+
+            OnDraw?.Invoke(this, new DrawEventArgs()
             {
-                g.DrawLines(pen, Points.ToArray());
-            }
+                Graphics = g,
+                Zoom = ZoomScale,
+                ViewPort = Origin
+            });
 
         }
 
+
+        /// <summary>
+        /// Handles mouse double clicks
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ZoomControl_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            if(e.Button == MouseButtons.Middle && e.Clicks > 1)
+            if (e.Button == MouseButtons.Middle && e.Clicks > 1)
             {
                 Reset();
                 return;
             }
         }
 
-
+        /// <summary>
+        /// Handle mouse wheel spin
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ZoomControl_MouseWheel(object sender, MouseEventArgs e)
         {
             var mouseX = e.Location.X;
@@ -169,6 +187,9 @@ namespace ZoomPanControl
             Invalidate();
         }
 
+        /// <summary>
+        /// Resets all transformations and zoom for this control
+        /// </summary>
         public void Reset()
         {
             Origin = new PointF(0, 0);
@@ -192,6 +213,13 @@ namespace ZoomPanControl
         {
             public double Zoom { get; set; }
 
+        }
+
+        public class DrawEventArgs : EventArgs
+        {
+            public Graphics Graphics { get; set; }
+            public double Zoom { get; set; }
+            public PointF ViewPort { get; set; }
         }
     }
 }
